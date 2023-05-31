27f4e69237b999f377c15847f63cfef120bd825e
Noam <cohennoam48@gmail.com>
2023-05-31 21:05:03 +0300

    bigger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 # Load the training and validation datasets
 train_data = pd.read_csv('data/train.csv', header=None)
@@ -34,11 +37,14 @@ def RELU(x):
 def RELU_derivative(x):
     return np.where(x > 0, 1, 0)
 
+
 def leaky_RELU(x):
     return np.maximum(x, 0.01 * x)
 
+
 def leaky_RELU_derivative(x):
     return np.where(x > 0, 1, 0.01)
+
 
 ActivationFunction = sigmoid
 ActivationFunctionDerivative = sigmoid_derivative
@@ -46,17 +52,20 @@ ActivationFunctionDerivative = sigmoid_derivative
 
 # Define the neural network model
 class NeuralNetwork2:
-    def __init__(self, input_size, hidden_size, output_size, dropout_prob, regularization):
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size, dropout_prob, regularization):
         self.input_size = input_size
-        self.hidden_size = hidden_size
+        self.hidden_size1 = hidden_size1
+        self.hidden_size2 = hidden_size2
         self.output_size = output_size
         self.dropout_prob = dropout_prob
         self.regularization = regularization
 
-        self.W1 = np.random.randn(self.input_size, self.hidden_size)
-        self.b1 = np.zeros((1, self.hidden_size))
-        self.W2 = np.random.randn(self.hidden_size, self.output_size)
-        self.b2 = np.zeros((1, self.output_size))
+        self.W1 = np.random.randn(self.input_size, self.hidden_size1)
+        self.b1 = np.zeros((1, self.hidden_size1))
+        self.W2 = np.random.randn(self.hidden_size1, self.hidden_size2)
+        self.b2 = np.zeros((1, self.hidden_size2))
+        self.W3 = np.random.randn(self.hidden_size2, self.output_size)
+        self.b3 = np.zeros((1, self.output_size))
 
     def forward(self, X, training):
         self.z1 = np.dot(X, self.W1) + self.b1
@@ -68,22 +77,42 @@ class NeuralNetwork2:
             self.a1 *= dropout_mask / (1 - self.dropout_prob)
 
         self.z2 = np.dot(self.a1, self.W2) + self.b2
-        self.a2 = self.softmax(self.z2)
-        return self.a2
+        self.a2 = ActivationFunction(self.z2)
+
+        # Apply dropout during training
+        if training:
+            dropout_mask = np.random.binomial(1, 1 - self.dropout_prob, size=self.a2.shape)
+            self.a2 *= dropout_mask / (1 - self.dropout_prob)
+
+        self.z3 = np.dot(self.a2, self.W3) + self.b3
+        self.a3 = self.softmax(self.z3)
+        return self.a3
 
     def backward(self, X, y, output, learning_rate):
-        delta2 = output - y
+        delta3 = output - y
+
+        dW3 = np.dot(self.a2.T, delta3) + self.regularization * self.W3
+        db3 = np.sum(delta3, axis=0, keepdims=True)
+
+        # Backpropagate dropout mask
+        dropout_mask2 = np.random.binomial(1, 1 - self.dropout_prob, size=self.a2.shape)
+        delta2 = np.dot(delta3, self.W3.T) * ActivationFunctionDerivative(self.z2) * dropout_mask2 / (
+                1 - self.dropout_prob)
+
         dW2 = np.dot(self.a1.T, delta2) + self.regularization * self.W2
         db2 = np.sum(delta2, axis=0, keepdims=True)
 
         # Backpropagate dropout mask
-        dropout_mask = np.random.binomial(1, 1 - self.dropout_prob, size=self.a1.shape)
+        dropout_mask1 = np.random.binomial(1, 1 - self.dropout_prob, size=self.a1.shape)
 
-        delta1 = np.dot(delta2, self.W2.T) * ActivationFunctionDerivative(self.z1) * dropout_mask / (1 - self.dropout_prob)
+        delta1 = np.dot(delta2, self.W2.T) * ActivationFunctionDerivative(self.z1) * dropout_mask1 / (
+                    1 - self.dropout_prob)
 
         dW1 = np.dot(X.T, delta1) + self.regularization * self.W1
         db1 = np.sum(delta1, axis=0, keepdims=True)
 
+        self.W3 -= learning_rate * dW3
+        self.b3 -= learning_rate * db3
         self.W2 -= learning_rate * dW2
         self.b2 -= learning_rate * db2
         self.W1 -= learning_rate * dW1
@@ -92,7 +121,7 @@ class NeuralNetwork2:
     def train(self, X, y, num_epochs, initial_learning_rate, batch_size, decay_rate):
         learning_rate = initial_learning_rate
 
-        for epoch in range(num_epochs):
+        for epoch in tqdm(range(num_epochs), file=sys.stdout):
             # Shuffle the training data
             indices = np.random.permutation(X.shape[0])
             X_shuffled = X[indices]
@@ -128,8 +157,6 @@ class NeuralNetwork2:
                 train_predictions = self.predict(X_train_subset)
                 train_accuracy = np.mean(train_predictions == np.argmax(y_train_subset, axis=1)) * 100
                 print(f"After {epoch + 1} epochs, Training Accuracy (Subset of 1000): {train_accuracy}%")
-
-
 
     def predict(self, X):
         output = self.forward(X, False)
@@ -173,11 +200,13 @@ def _pre_processing(X: np.ndarray, Y: np.ndarray, noise_std: float = 0.1):
 
 def main():
     np.random.seed(10)
+    np.seterr(invalid="raise")
     # Set the hyperparameters
     input_size = X_train.shape[1]
-    hidden_size = 512
+    hidden_size1 = 512
+    hidden_size2 = 256
     output_size = 10
-    num_epochs = 60
+    num_epochs = 100
     init_learning_rate = 0.05
     learning_rate_decay = 0.9
     batch_size = 64
@@ -185,7 +214,7 @@ def main():
     regularization = 0
 
     # Create the neural network model
-    model = NeuralNetwork2(input_size, hidden_size, output_size, dropout_prob, regularization)
+    model = NeuralNetwork2(input_size, hidden_size1, hidden_size2, output_size, dropout_prob, regularization)
 
     # Train the neural network
     train_x, train_y = _pre_processing(X_train, y_train_encoded)
