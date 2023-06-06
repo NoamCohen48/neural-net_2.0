f5eeb409044931bba04b112f73e2e9f7bb229e76
Noam <cohennoam48@gmail.com>
2023-06-06 14:35:26 +0300

    added normelize and more data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pickle
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the training and validation datasets
 train_data = pd.read_csv('data/train.csv', header=None)
@@ -12,9 +13,6 @@ X_train = train_data.iloc[:, 1:].values
 y_train = train_data.iloc[:, 0].values - 1
 X_validate = validate_data.iloc[:, 1:].values
 y_validate = validate_data.iloc[:, 0].values - 1
-
-# y_train = y_train - 1
-# y_validate = y_validate - 1
 
 # Convert labels to one-hot encoding
 y_train_encoded = np.eye(10)[y_train]
@@ -62,6 +60,17 @@ class NeuralNetwork2:
         self.W2 = np.random.randn(self.hidden_size, self.output_size) / np.sqrt(self.hidden_size)
         self.b2 = np.zeros((1, self.output_size))
 
+        self.train_accuracy = []
+        self.valid_accuracy = []
+        self.epochs = []
+
+        plt.ion()
+        # Create a figure and axis for the plot
+        fig, ax = plt.subplots()
+        ax.set_xlabel('X')
+        ax.set_ylabel('accuracy')
+        ax.set_title('Data Points')
+
     def forward(self, X, training):
         self.z1 = np.dot(X, self.W1) + self.b1
         self.a1 = ActivationFunction(self.z1)
@@ -82,11 +91,11 @@ class NeuralNetwork2:
         dW2 = np.dot(self.a1.T, delta2) / m
         db2 = np.sum(delta2, axis=0, keepdims=True) / m
 
-        # Backpropagate dropout mask
+        # Backpropagation dropout mask
         dropout_mask = np.random.binomial(1, 1 - self.dropout_prob, size=self.a1.shape)
 
         delta1 = np.dot(delta2, self.W2.T) * ActivationFunctionDerivative(self.z1) * dropout_mask / (
-                    1 - self.dropout_prob)
+                1 - self.dropout_prob)
 
         dW1 = np.dot(X.T, delta1) / m
         db1 = np.sum(delta1, axis=0, keepdims=True) / m
@@ -133,8 +142,17 @@ class NeuralNetwork2:
                 y_train_subset = y[random_indices]
                 train_predictions = self.predict(X_train_subset)
                 train_accuracy = np.mean(train_predictions == np.argmax(y_train_subset, axis=1)) * 100
+
+                self.train_accuracy.append(train_accuracy)
+                self.valid_accuracy.append(validate_accuracy)
+                self.epochs.append(epoch)
+
                 print(f"After {epoch + 1} epochs, Training Accuracy (Subset of 1000): {train_accuracy}%")
                 print(f"After {epoch + 1} epochs, Validation Accuracy : {validate_accuracy}%")
+
+        self.plot()
+        plt.ioff()
+        plt.show()
 
     def predict(self, X):
         output = self.forward(X, False)
@@ -144,25 +162,47 @@ class NeuralNetwork2:
         exps = np.exp(x - np.max(x, axis=1, keepdims=True))
         return exps / np.sum(exps, axis=1, keepdims=True)
 
+    def plot(self):
+        plt.scatter(self.epochs, self.train_accuracy, color='red', s=50)
+        plt.plot(self.epochs, self.train_accuracy)
+        for x_val, y_val in zip(self.epochs, self.train_accuracy):
+            plt.text(x_val, y_val + 1, f'{y_val:.2f}', color='black', ha='center')
+        plt.pause(0.1)
+
+
+def show_image(x):
+    plt.imshow(x)
+    plt.title("RGB Image")
+    plt.xlabel("Width")
+    plt.ylabel("Height")
+    plt.colorbar()
+    plt.axis("off")  # Remove the axis labels and ticks
+    plt.show()
+
 
 def _pre_processing(X: np.ndarray, Y: np.ndarray, reset_percentage: float = 0.2, noise_std: float = 0.1):
-    min_vals = np.min(X, axis=1)
-    max_vals = np.max(X, axis=1)
-    # Normalize each image individually using min-max normalization
-    X = (X - min_vals[:, np.newaxis]) / (max_vals - min_vals)[:, np.newaxis]
+    # min_vals = np.min(X, axis=1)
+    # max_vals = np.max(X, axis=1)
+    # # Normalize each image individually using min-max normalization
+    # X = (X - min_vals[:, np.newaxis]) / (max_vals - min_vals)[:, np.newaxis]
 
-    X_reshaped = X.reshape((-1, 3, 32, 32))
+    X_reshaped = X.reshape((-1, 32, 32, 3))
+    show_image(X_reshaped[0])
+
+    mean = np.mean(X_reshaped, axis=(0, 1, 2), keepdims=True)
+    std = np.std(X_reshaped, axis=(0, 1, 2), keepdims=True)
+    X_reshaped = (X_reshaped - mean) / std
 
     # Flipping Images horizontally
-    horizontal_flipped = np.flip(X_reshaped, axis=3)
+    horizontal_flipped = np.flip(X_reshaped, axis=2)
     horizontal_flipped = horizontal_flipped.reshape(X.shape[0], -1)
 
     # Flipping Images vertically
-    vertically_flipped = np.flip(X_reshaped, axis=2)
+    vertically_flipped = np.flip(X_reshaped, axis=1)
     vertically_flipped = vertically_flipped.reshape(X.shape[0], -1)
 
     # Flipping Images vertically and horizontally
-    v_h_flipped = np.flip(X_reshaped, axis=(2, 3))
+    v_h_flipped = np.flip(X_reshaped, axis=(1, 2))
     v_h_flipped = v_h_flipped.reshape(X.shape[0], -1)
 
     X_new = np.concatenate((X, horizontal_flipped, vertically_flipped, v_h_flipped))
@@ -209,9 +249,9 @@ def main():
     model = NeuralNetwork2(input_size, hidden_size, output_size, dropout_prob)
 
     # Train the neural network
-    train_x, train_y = _pre_processing(X_train, y_train_encoded)
+    # train_x, train_y = _pre_processing(X_train, y_train_encoded)
 
-    model.train(train_x, train_y, num_epochs, init_learning_rate, batch_size, learning_rate_decay)
+    model.train(X_train[:100], y_train_encoded[:100], num_epochs, init_learning_rate, batch_size, learning_rate_decay)
 
     # Make predictions on the training set
     train_predictions = model.predict(X_train)
