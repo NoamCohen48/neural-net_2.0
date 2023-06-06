fb79d1278132e4c131cc55630e4563c0c2f371cc
Noam <cohennoam48@gmail.com>
2023-06-06 19:25:02 +0300

    fixed test save

--- main.py
+++ main.py
@@ -5,20 +5,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the training and validation datasets
-train_data = pd.read_csv(f"{sys.argv[1]}/train.csv", header=None)
-validate_data = pd.read_csv(f"{sys.argv[1]}/validate.csv", header=None)
-
-# Separate the features and labels
-X_train = train_data.iloc[:, 1:].values
-y_train = train_data.iloc[:, 0].values - 1
-X_validate = validate_data.iloc[:, 1:].values
-y_validate = validate_data.iloc[:, 0].values - 1
-
-# Convert labels to one-hot encoding
-y_train_encoded = np.eye(10)[y_train]
-y_validate_encoded = np.eye(10)[y_validate]
-
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -106,16 +92,16 @@ class NeuralNetwork2:
         self.W1 -= learning_rate * dW1
         self.b1 -= learning_rate * db1
 
-    def train(self, X, y, num_epochs, initial_learning_rate, batch_size, decay_rate):
+    def train(self, X_train, Y_train, X_valid, Y_valid, num_epochs, initial_learning_rate, batch_size, decay_rate):
         learning_rate = initial_learning_rate
 
         for epoch in range(num_epochs):
             # Shuffle the training data
-            indices = np.random.permutation(X.shape[0])
-            X_shuffled = X[indices]
-            y_shuffled = y[indices]
+            indices = np.random.permutation(X_train.shape[0])
+            X_shuffled = X_train[indices]
+            y_shuffled = Y_train[indices]
 
-            for batch_start in range(0, X.shape[0], batch_size):
+            for batch_start in range(0, X_train.shape[0], batch_size):
                 # Divide the data into mini-batches
                 batch_end = batch_start + batch_size
                 X_batch = X_shuffled[batch_start:batch_end]
@@ -134,13 +120,13 @@ class NeuralNetwork2:
 
             # Print progress every 10 epochs
             if (epoch + 1) % 10 == 0:
-                validate_predictions = self.predict(X_validate)
-                validate_accuracy = np.mean(validate_predictions == np.argmax(y_validate_encoded, axis=1)) * 100
+                validate_predictions = self.predict(X_valid)
+                validate_accuracy = np.mean(validate_predictions == np.argmax(Y_valid, axis=1)) * 100
 
                 # Calculate training accuracy on a random subset of 1000 different examples
-                random_indices = np.random.choice(X.shape[0], size=1000)
-                X_train_subset = X[random_indices]
-                y_train_subset = y[random_indices]
+                random_indices = np.random.choice(X_train.shape[0], size=1000)
+                X_train_subset = X_train[random_indices]
+                y_train_subset = Y_train[random_indices]
                 train_predictions = self.predict(X_train_subset)
                 train_accuracy = np.mean(train_predictions == np.argmax(y_train_subset, axis=1)) * 100
 
@@ -167,12 +153,12 @@ class NeuralNetwork2:
         plt.scatter(self.epochs, self.train_accuracy, color='red', s=50)
         plt.plot(self.epochs, self.train_accuracy)
         for x_val, y_val in zip(self.epochs, self.train_accuracy):
-            plt.text(x_val+0.5, y_val + 1, f'{y_val:.2f}', color='black', ha='center')
+            plt.text(x_val + 0.5, y_val + 1, f'{y_val:.2f}', color='black', ha='center')
 
         plt.scatter(self.epochs, self.valid_accuracy, color='red', s=50)
         plt.plot(self.epochs, self.valid_accuracy)
         for x_val, y_val in zip(self.epochs, self.valid_accuracy):
-            plt.text(x_val+0.5, y_val + 1, f'{y_val:.2f}', color='black', ha='center')
+            plt.text(x_val + 0.5, y_val + 1, f'{y_val:.2f}', color='black', ha='center')
         plt.pause(0.1)
 
 
@@ -182,23 +168,44 @@ def show_image(x):
     plt.title("RGB Image")
     plt.xlabel("Width")
     plt.ylabel("Height")
-    plt.colorbar()
     plt.axis("off")  # Remove the axis labels and ticks
     plt.show()
 
 
-def _pre_processing(X: np.ndarray, Y: np.ndarray, reset_percentage: float = 0.2, noise_std: float = 0.1):
+def load_data():
+    # Load the training and validation datasets
+    train_data = pd.read_csv(f"{sys.argv[1]}/train.csv", header=None)
+    validate_data = pd.read_csv(f"{sys.argv[1]}/validate.csv", header=None)
+
+    # Separate the features and labels
+    X_train = train_data.iloc[:, 1:].values
+    y_train = train_data.iloc[:, 0].values - 1
+    X_validate = validate_data.iloc[:, 1:].values
+    y_validate = validate_data.iloc[:, 0].values - 1
+
+    # Convert labels to one-hot encoding
+    y_train_encoded = np.eye(10)[y_train]
+    y_validate_encoded = np.eye(10)[y_validate]
+
+    return X_train, y_train_encoded, X_validate, y_validate_encoded
+
+
+def normalize(X: np.ndarray):
+    # ~~~ min-max normalization ~~~
     # min_vals = np.min(X, axis=1)
     # max_vals = np.max(X, axis=1)
-    # # Normalize each image individually using min-max normalization
-    # X = (X - min_vals[:, np.newaxis]) / (max_vals - min_vals)[:, np.newaxis]
+    # return (X - min_vals[:, np.newaxis]) / (max_vals - min_vals)[:, np.newaxis]
 
+    # ~~~ Normal normalization ~~~
     X_reshaped = X.reshape((-1, 3, 32, 32))
-    show_image(X_reshaped[0])
-
     mean = np.mean(X_reshaped, axis=(0, 2, 3), keepdims=True)
     std = np.std(X_reshaped, axis=(0, 2, 3), keepdims=True)
     X_reshaped = (X_reshaped - mean) / std
+    return X_reshaped.reshape((-1, 3072))
+
+
+def data_augmentation(X: np.ndarray, Y: np.ndarray, reset_percentage: float = 0.2, noise_std: float = 0.1):
+    X_reshaped = X.reshape((-1, 3, 32, 32))
 
     # Flipping Images horizontally
     horizontal_flipped = np.flip(X_reshaped, axis=3)
@@ -242,8 +249,9 @@ def load_model():
 def main():
     np.random.seed(10)
     np.seterr(all="raise")
+
     # Set the hyperparameters
-    input_size = X_train.shape[1]
+    input_size = 3072
     hidden_size = 256
     output_size = 10
     num_epochs = 100
@@ -252,22 +260,27 @@ def main():
     batch_size = 32
     dropout_prob = 0.2
 
+    X_train, y_train, X_validate, y_validate = load_data()
+    X_train = normalize(X_train)
+    X_validate = normalize(X_validate)
+
     # Create the neural network model
     model = NeuralNetwork2(input_size, hidden_size, output_size, dropout_prob)
 
     # Train the neural network
-    train_x, train_y = _pre_processing(X_train, y_train_encoded)
+    train_x, train_y = data_augmentation(X_train, y_train)
 
-    model.train(train_x, train_y, num_epochs, init_learning_rate, batch_size, learning_rate_decay)
+    model.train(train_x, train_y, X_validate, y_validate,
+                num_epochs, init_learning_rate, batch_size, learning_rate_decay)
 
     # Make predictions on the training set
     train_predictions = model.predict(X_train)
-    train_accuracy = np.mean(train_predictions == np.argmax(y_train_encoded, axis=1)) * 100
+    train_accuracy = np.mean(train_predictions == np.argmax(y_train, axis=1)) * 100
     print(f"Training Accuracy at the end {train_accuracy}%")
 
     # Make predictions on the validation set
     validate_predictions = model.predict(X_validate)
-    validate_accuracy = np.mean(validate_predictions == np.argmax(y_validate_encoded, axis=1)) * 100
+    validate_accuracy = np.mean(validate_predictions == np.argmax(y_validate, axis=1)) * 100
     print(f"Validation Accuracy: at the end {validate_accuracy}%")
 
     # Save predictions on the Test set
